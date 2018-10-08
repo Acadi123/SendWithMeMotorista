9e58c9010558af497bb93c4e578db4c1b45234ff
Acadi123 <ac.adi@hotmail.com>
2018-10-08 00:33:43 -0300

    atualização cadastro

--- app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/CadastroActivity.java
+++ app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/CadastroActivity.java
@@ -2,35 +2,66 @@ package projetosendwithmemotorista.sendwithmemotorista.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import projetosendwithmemotorista.sendwithmemotorista.Entidades.Usuarios;
 import projetosendwithmemotorista.sendwithmemotorista.R;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private EditText edtCardEmail;
-    private EditText edtCardNome;
-    private EditText edtCardSobrenome;
-    private EditText edtCardSenha;
-    private EditText edtCardDataNascimento;
+    private EditText edtCadEmail;
+    private EditText edtCadNome;
+    private EditText edtCadSobrenome;
+    private EditText edtCadSenha;
+    private EditText edtCadConfirmarSenha;
+    private EditText edtCadDataNascimento;
     private RadioButton rbMasculino;
     private RadioButton rbFeminino;
     private Button btnSalvar;
+    private Usuarios usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        edtCardEmail = (EditText)findViewById(R.id.edtCardEmail);
-        edtCardNome = (EditText)findViewById(R.id.edtCardNome);
-        edtCardSobrenome = (EditText)findViewById(R.id.edtCardSobrenome);
-        edtCardSenha = (EditText)findViewById(R.id.edtCardSenha);
-        edtCardDataNascimento = (EditText)findViewById(R.id.edtCardDataNascimento);
+        edtCadEmail = (EditText)findViewById(R.id.edtCadEmail);
+        edtCadNome = (EditText)findViewById(R.id.edtCadNome);
+        edtCadSobrenome = (EditText)findViewById(R.id.edtCadSobrenome);
+        edtCadSenha = (EditText)findViewById(R.id.edtCadSenha);
+        edtCadConfirmarSenha = (EditText)findViewById(R.id.edtCadConfirmarSenha);
+        edtCadDataNascimento = (EditText)findViewById(R.id.edtCadDataNascimento);
         rbFeminino = (RadioButton) findViewById(R.id.rbFeminino);
         rbMasculino = (RadioButton) findViewById(R.id.rbMasculino);
+        btnSalvar = (Button) findViewById(R.id.btnSalvar);
 
+
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtCadSenha.getText().toString().equals(edtCadConfirmarSenha.getText().toString())){
+                    usuarios = new Usuarios();
+                    usuarios.setNome(edtCadNome.getText().toString());
+                    usuarios.setEmail(edtCadEmail.getText().toString());
+                    usuarios.setSenha(edtCadSenha.getText().toString());
+                    usuarios.setNascimento(edtCadDataNascimento.getText().toString());
+                    usuarios.setSobrenome(edtCadSobrenome.getText().toString());
+
+                    if (rbFeminino.isChecked()) {
+                        usuarios.setSexo("Feminino");
+                    } else {
+                        usuarios.setSexo("Masculino");
+                    }
+
+                }else {
+                    Toast.makeText(CadastroActivity.this, "As senhas não são correspondentes", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
