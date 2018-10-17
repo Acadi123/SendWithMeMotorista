985b067e734fccc7dcce4363f2eb785e321ec57a
Dimitrimm <40797429+Dimitrimm@users.noreply.github.com>
2018-10-17 11:04:07 -0300

    Tela principal (map,card)

--- app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/PrincipalActivity.java
+++ app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/PrincipalActivity.java
@@ -2,14 +2,28 @@ package projetosendwithmemotorista.sendwithmemotorista.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import projetosendwithmemotorista.sendwithmemotorista.R;
 
-public class PrincipalActivity extends AppCompatActivity {
+public class PrincipalActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    MapView mapView;
+    View mVies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
