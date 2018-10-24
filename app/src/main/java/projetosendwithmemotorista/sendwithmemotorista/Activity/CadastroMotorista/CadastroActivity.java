8857503725adcf4fe668e0ef464eedbdfedbcac6
Acadi <ac.adi@hotmail.com>
2018-10-24 15:10:28 -0300

    atualização organização

--- app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/CadastroMotorista/CadastroActivity.java
+++ app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/CadastroMotorista/CadastroActivity.java
@@ -1,10 +1,9 @@
-package projetosendwithmemotorista.sendwithmemotorista.Activity;
+package projetosendwithmemotorista.sendwithmemotorista.Activity.CadastroMotorista;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.system.ErrnoException;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 
+import projetosendwithmemotorista.sendwithmemotorista.Activity.LoginMotorista.LoginActivity;
 import projetosendwithmemotorista.sendwithmemotorista.DAO.ConfiguracaoFirebase;
 import projetosendwithmemotorista.sendwithmemotorista.Entidades.Usuarios;
 import projetosendwithmemotorista.sendwithmemotorista.Helper.Base64Custom;
