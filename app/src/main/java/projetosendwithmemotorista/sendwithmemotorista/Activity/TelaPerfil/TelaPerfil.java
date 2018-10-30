b2b6cce9f478618128a87bfbac33968d1abb6cdf
Acadi Teofilo <ac.adi@hotmail.com>
2018-10-30 12:36:50 -0300

    Editar Perfil

--- app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/TelaPerfil/TelaPerfil.java
+++ app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/TelaPerfil/TelaPerfil.java
@@ -1,11 +1,14 @@
 package projetosendwithmemotorista.sendwithmemotorista.Activity.TelaPerfil;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import projetosendwithmemotorista.sendwithmemotorista.Activity.LoginMotorista.LoginActivity;
 import projetosendwithmemotorista.sendwithmemotorista.Activity.TelaPrincipalMapa.PrincipalActivity;
 import projetosendwithmemotorista.sendwithmemotorista.Entidades.Usuarios;
 import projetosendwithmemotorista.sendwithmemotorista.Helper.PreferenciasAndroid;
@@ -29,7 +33,9 @@ public class TelaPerfil extends AppCompatActivity {
     private TextView emailPerfil;
     private TextView cpfPerfil;
     private TextView dataPerfil;
+    private TextView senhaPerfil;
     private Button voltaMapa;
+    private DatabaseReference mDatebaseRef;
 
 
     @Override
@@ -38,12 +44,16 @@ public class TelaPerfil extends AppCompatActivity {
         setContentView(R.layout.activity_tela_perfil);
         setarDadosPerfil();
 
+
         btnExcluirConta = (Button) findViewById(R.id.EditarPerfil);
         btnExcluirConta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentAbrirTelaPrincipal = new Intent(TelaPerfil.this, EditarPerfil.class);
-                startActivity(intentAbrirTelaPrincipal);
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                user.delete();
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(TelaPerfil.this, LoginActivity.class));
+                TelaPerfil.this.finish();
             }
         });
         voltaMapa = (Button) findViewById(R.id.voltaMapa);
@@ -67,6 +77,11 @@ public class TelaPerfil extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
               Usuarios usuarios = dataSnapshot.getValue(Usuarios.class);
               setView();
+              EditarNome();
+              EditarEmail();
+              EditarCpf();
+              EditarData();
+              EditarSenha();
               nomePerfil.setText(usuarios.getNome());
               emailPerfil.setText(usuarios.getEmail());
               cpfPerfil.setText(usuarios.getCpf());
@@ -86,6 +101,250 @@ public class TelaPerfil extends AppCompatActivity {
         emailPerfil = findViewById(R.id.emailPerfilId);
         cpfPerfil = findViewById(R.id.cpfPerfilId);
         dataPerfil = findViewById(R.id.dataPerfilId);
+        senhaPerfil = findViewById(R.id.senhaPerfilId);
+
+    }
+
+    private void EditarNome(){
+        nomePerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(TelaPerfil.this);
+                View alertview = getLayoutInflater().inflate(R.layout.dialogo_editar_perfil, null);
+                TextView alertTitulo = alertview.findViewById(R.id.textViewEditar);
+                final EditText editTextAlterar = alertview.findViewById(R.id.editTextAlertConf);
+                Button buttonConfirmar = alertview.findViewById(R.id.bttEdicao);
+                Button buttonCancelar = alertview.findViewById(R.id.bttCancelarEdicao);
+
+                alertTitulo.setText("Digite seu Nome: ");
+                buttonConfirmar.setText("Editar");
+                buttonCancelar.setText("Cancelar");
+                alertDialog.setView(alertview);
+                final AlertDialog dialog = alertDialog.create();
+                dialog.show();
+
+                buttonCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+
+                    }
+                });
+
+
+
+
+
+
+
+                buttonConfirmar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View v) {
+                        if (!editTextAlterar.toString().isEmpty()) {
+
+                            PreferenciasAndroid preferenciasAndroid = new PreferenciasAndroid(TelaPerfil.this);
+                            mDatebaseRef = FirebaseDatabase.getInstance().getReference("usuario").child(preferenciasAndroid.getIdentificador()).child("nome"); //mudar o child para cada informacao correspondente
+                            mDatebaseRef.setValue(editTextAlterar.getText().toString());
+                            setarDadosPerfil();
+                            Toast.makeText(TelaPerfil.this, "Nome alterado", Toast.LENGTH_LONG).show();
+                            dialog.dismiss();
+
+                        }
+
+
+                    }
+                });
+
+
+            }
+        });
+
+    }
+
+
+    private void EditarEmail(){
+        emailPerfil.setOnClickListener(new View.OnClickListener() { //Alterar
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(TelaPerfil.this, "Você não pode alterar o Email", Toast.LENGTH_LONG).show(); //Alterar
+
+
+            }
+        });
+
+    }
+
+    private void EditarCpf(){ //chamarMetodo no oncreate
+        cpfPerfil.setOnClickListener(new View.OnClickListener() { //Alterar
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(TelaPerfil.this);
+                View alertview = getLayoutInflater().inflate(R.layout.dialogo_editar_perfil, null);
+                TextView alertTitulo = alertview.findViewById(R.id.textViewEditar);
+                final EditText editTextAlterar = alertview.findViewById(R.id.editTextAlertConf);
+                Button buttonConfirmar = alertview.findViewById(R.id.bttEdicao);
+                Button buttonCancelar = alertview.findViewById(R.id.bttCancelarEdicao);
+
+                alertTitulo.setText("Digite seu CPF: "); //Alterar
+                buttonConfirmar.setText("Editar");
+                buttonCancelar.setText("Cancelar");
+                alertDialog.setView(alertview);
+                final AlertDialog dialog = alertDialog.create();
+                dialog.show();
+
+                buttonCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+
+                    }
+                });
+
+
+
+
+
+
+
+                buttonConfirmar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View v) {
+                        if (!editTextAlterar.toString().isEmpty()) {
+
+                            PreferenciasAndroid preferenciasAndroid = new PreferenciasAndroid(TelaPerfil.this);
+                            mDatebaseRef = FirebaseDatabase.getInstance().getReference("usuario").child(preferenciasAndroid.getIdentificador()).child("cpf"); //mudar o child para cada informacao correspondente
+                            mDatebaseRef.setValue(editTextAlterar.getText().toString());
+                            setarDadosPerfil();
+                            Toast.makeText(TelaPerfil.this, "CPF alterado", Toast.LENGTH_LONG).show(); //Alterar
+                            dialog.dismiss();
+
+                        }
+
+
+                    }
+                });
+
+
+            }
+        });
+
+    }
+
+    private void EditarData(){
+        dataPerfil.setOnClickListener(new View.OnClickListener() { //Alterar
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(TelaPerfil.this);
+                View alertview = getLayoutInflater().inflate(R.layout.dialogo_editar_perfil, null);
+                TextView alertTitulo = alertview.findViewById(R.id.textViewEditar);
+                final EditText editTextAlterar = alertview.findViewById(R.id.editTextAlertConf);
+                Button buttonConfirmar = alertview.findViewById(R.id.bttEdicao);
+                Button buttonCancelar = alertview.findViewById(R.id.bttCancelarEdicao);
+
+                alertTitulo.setText("Digite sua data de nascimento: "); //Alterar
+                buttonConfirmar.setText("Editar");
+                buttonCancelar.setText("Cancelar");
+                alertDialog.setView(alertview);
+                final AlertDialog dialog = alertDialog.create();
+                dialog.show();
+
+                buttonCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+
+                    }
+                });
+
+
+
+
+
+
+
+                buttonConfirmar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View v) {
+                        if (!editTextAlterar.toString().isEmpty()) {
+
+                            PreferenciasAndroid preferenciasAndroid = new PreferenciasAndroid(TelaPerfil.this);
+                            mDatebaseRef = FirebaseDatabase.getInstance().getReference("usuario").child(preferenciasAndroid.getIdentificador()).child("nascimento"); //mudar o child para cada informacao correspondente
+                            mDatebaseRef.setValue(editTextAlterar.getText().toString());
+                            setarDadosPerfil();
+                            Toast.makeText(TelaPerfil.this, "Data de nascimento alterada", Toast.LENGTH_LONG).show(); //Alterar
+                            dialog.dismiss();
+
+                        }
+
+
+                    }
+                });
+
+
+            }
+        });
+
+    }
+
+    private void EditarSenha(){//chmar no oncreate
+        senhaPerfil.setOnClickListener(new View.OnClickListener() { //Alterar
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(TelaPerfil.this);
+                View alertview = getLayoutInflater().inflate(R.layout.dialogo_editar_perfil, null);
+                TextView alertTitulo = alertview.findViewById(R.id.textViewEditar);
+                final EditText editTextAlterar = alertview.findViewById(R.id.editTextAlertConf);
+                Button buttonConfirmar = alertview.findViewById(R.id.bttEdicao);
+                Button buttonCancelar = alertview.findViewById(R.id.bttCancelarEdicao);
+
+                alertTitulo.setText("Digite sua senha: "); //Alterar
+                buttonConfirmar.setText("Editar");
+                buttonCancelar.setText("Cancelar");
+                alertDialog.setView(alertview);
+                final AlertDialog dialog = alertDialog.create();
+                dialog.show();
+
+                buttonCancelar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+
+                    }
+                });
+
+
+
+
+
+
+
+                buttonConfirmar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View v) {
+                        if (!editTextAlterar.toString().isEmpty()) {
+
+                            PreferenciasAndroid preferenciasAndroid = new PreferenciasAndroid(TelaPerfil.this);
+                            mDatebaseRef = FirebaseDatabase.getInstance().getReference("usuario").child(preferenciasAndroid.getIdentificador()).child("senha"); //mudar o child para cada informacao correspondente
+                            mDatebaseRef.setValue(editTextAlterar.getText().toString());
+                            final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            user.updatePassword(editTextAlterar.getText().toString());
+                            setarDadosPerfil();
+                            Toast.makeText(TelaPerfil.this, "Senha alterada", Toast.LENGTH_LONG).show();
+                            dialog.dismiss();
+
+                        }
+
+
+                    }
+                });
+
+
+            }
+        });
 
     }
 
